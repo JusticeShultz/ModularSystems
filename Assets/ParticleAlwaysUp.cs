6f82548fc1bc55c2c7b292a76b01fc607f713436
Justice Shultz <s189081@students.aie.edu>
2019-10-02 13:01:02 -0700

    Made some changes to smooth things up

---
+++ Assets/ParticleAlwaysUp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleAlwaysUp : MonoBehaviour
+{
+    public GameObject particle;
+
+	void Update ()
+    {
+        particle.transform.rotation = Quaternion.Euler(-90, 0, 0);
+	}
+}
